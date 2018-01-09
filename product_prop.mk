b1df5845128c8a6877102fbf60c0dcfb9042a36a
Suren Baghdasaryan <surenb@google.com>
2018-01-10 07:47:00 +0800

    davinci: Set lmkd configuration properties
    Bug: 63631020
    
    Change-Id: I16a183e8e4625e8b98cefdc9c8a7b650556a2b55
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- product_prop.mk
+++ product_prop.mk
@@ -12,6 +12,18 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     vendor.display.disable_hw_recovery_dump=0
 
+# LMKD
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=0 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
+
 # OEM
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.oem_unlock_supported=0
