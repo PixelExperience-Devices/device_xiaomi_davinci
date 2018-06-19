b2f209050f04e834e4110fffd494e1a6c9612ed4
Jorim Jaggi <jjaggi@google.com>
2018-06-19 17:07:02 +0200

    davinci: sysprop: Refined early SF configuration for WCG
    Test: Use photos, observe no jank
    Bug: 110112323
    Change-Id: Ib331d0b792ee1790963159a17ec24f36b4a1882a
    Merged-In: Ib331d0b792ee1790963159a17ec24f36b4a1882a

--- system_prop.mk
+++ system_prop.mk
@@ -48,7 +48,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
-    debug.sf.early_phase_offset_ns=5000000 \
+    debug.sf.early_app_phase_offset_ns=500000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_phase_offset_ns=500000 \
     debug.sf.enable_hwc_vds=1
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
