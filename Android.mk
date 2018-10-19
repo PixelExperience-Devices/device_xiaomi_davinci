3389e6e68fc89b75fe371b4e80a5f6d3df0fdea4
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-10-19 17:11:10 +0200

    davinci: Make Android.mk search recursive
    Change-Id: I3a05344f949eae581f1f17cc33b1df475d3e6760

--- Android.mk
+++ Android.mk
@@ -16,5 +16,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter davinci,$(TARGET_DEVICE)),)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
