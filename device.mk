4264ecc9552ba18d757a8856927347d3ccbf5651
weivincewang <wvw@google.com>
2019-03-29 00:23:41 -0400

    davinci: switch to libperfmgr for powerhint
    Bug: 129495482
    Bug: 111098032
    Test: Boot and check PowerHAL
    Change-Id: I79b7276ff0c28885877af8b0533fe1f9d81b3b0c

--- device.mk
+++ device.mk
@@ -138,7 +138,11 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.davinci
+    android.hardware.power@1.3-service.davinci-libperfmgr \
+    powerhint.json
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/perf/perf-profile0.conf
 
 # Telephony
 PRODUCT_PACKAGES += \
