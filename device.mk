e9b131faff7221ac03365f8083d56fb5d93081e3
LuK1337 <priv.luk@gmail.com>
2019-09-10 04:18:46 +0200

    davinci: Let system read and write to FRP block device
    * No clue why this is needed but ok.
    
    Change-Id: Ib40ab18e42ced8995f57c9d0a8e1a6decdc7f146

--- device.mk
+++ device.mk
@@ -56,7 +56,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.mi_thermald.rc \
     init.qcom.rc \
-    init.safailnet.rc
+    init.safailnet.rc \
+    ueventd.qcom.rc
 
 # Display
 PRODUCT_PACKAGES += \
