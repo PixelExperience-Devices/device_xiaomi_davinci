e63ea72996df601fab73a236a006f0783f32d270
Patrick Tjin <pattjin@google.com>
2019-10-15 10:32:37 +0800

    davinci: Add nosdcard characteristic to device
    * Just as per stock.
    
    Bug: 30895189
    Change-Id: Ic2e040dc8d811ca84c910d42f6f7db35526802ff
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- device.mk
+++ device.mk
@@ -21,6 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+PRODUCT_CHARACTERISTICS := nosdcard
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/davinci/davinci-vendor.mk)
 
