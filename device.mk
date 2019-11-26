585fc3ccfc0f26546e4004453a04989432875eb4
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-11-26 18:21:12 +0000

    davinci: Switch to system side basic USB hal
    Forked from lineage/interfaces

--- device.mk
+++ device.mk
@@ -149,6 +149,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.davinci
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl
