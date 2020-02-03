f8619ddcde1468da57304d5690a6c9394867e08d
raysenlau <raysen@live.cn>
2020-02-03 21:48:57 +0800

    davinci: Import audio policy configuration from stock
     * From Xiaomi/davinci/davinci:10/QKQ1.190825.002/V11.0.4.0.QFJMIXM:user/release-keys

--- device.mk
+++ device.mk
@@ -62,6 +62,10 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     libaacwrapper
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap
