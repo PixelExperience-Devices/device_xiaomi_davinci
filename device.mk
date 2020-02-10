Davinci-Android-Development/android_device_xiaomi_davinci

davinci: Introduce 'SafailNet'
Bypass safetynet even if we have permissive selinux

Change-Id: Ib44cdbd15b53af3a1768cb0912f2dbcc56a8d97e

device.mk
@@ -47,7 +47,8 @@ PRODUCT_PACKAGES += \
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.safailnet.rc
 
 # Display
 PRODUCT_PACKAGES += \

PixelExperience-Devices/device_xiaomi_davinci

device.mk
@@ -54,7 +54,9 @@ PRODUCT_PACKAGES += \
 
 # Common init scripts
 PRODUCT_PACKAGES += \
-    init.mi_thermald.rc
+    init.mi_thermald.rc \
+    init.qcom.rc \
+    init.safailnet.rc
 
 # Display
 PRODUCT_PACKAGES += \