Davinci-Android-Development/android_device_xiaomi_davinci

davinci: Start using vendor_overlay
* This lets us override files in /vendor without
  relying on bind mounts ^.^

Change-Id: Ib0b173682a48836aeac97e2c3691176a44f13aab

light/Android.mk
@@ -1,0 +1,42 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.light@2.0-service.xiaomi_davinci
+LOCAL_MODULE_TAGS  := optional
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_STEM := android.hardware.light@2.0-service
+
+LOCAL_SRC_FILES := \
+    service.cpp \
+    Light.cpp
+
+LOCAL_REQUIRED_MODULES := \
+    android.hardware.light@2.0-service.xiaomi_davinci.rc
+
+LOCAL_SHARED_LIBRARIES := \
+    libbase \
+    libhardware \
+    libhidlbase \
+    libhidltransport \
+    liblog \
+    libhwbinder \
+    libutils \
+    android.hardware.light@2.0
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.light@2.0-service.xiaomi_davinci.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
+LOCAL_MODULE_STEM := android.hardware.light@2.0-service.rc
+
+LOCAL_SRC_FILES := android.hardware.light@2.0-service.xiaomi_davinci.rc
+
+include $(BUILD_PREBUILT)

PixelExperience-Devices/device_xiaomi_davinci

light/Android.mk
@@ -1,0 +1,42 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.light@2.0-service.davinci
+LOCAL_MODULE_TAGS  := optional
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_STEM := android.hardware.light@2.0-service
+
+LOCAL_SRC_FILES := \
+    service.cpp \
+    Light.cpp
+
+LOCAL_REQUIRED_MODULES := \
+    android.hardware.light@2.0-service.davinci.rc
+
+LOCAL_SHARED_LIBRARIES := \
+    libbase \
+    libhardware \
+    libhidlbase \
+    libhidltransport \
+    liblog \
+    libhwbinder \
+    libutils \
+    android.hardware.light@2.0
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := android.hardware.light@2.0-service.davinci.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/init
+LOCAL_MODULE_STEM := android.hardware.light@2.0-service.rc
+
+LOCAL_SRC_FILES := android.hardware.light@2.0-service.davinci.rc
+
+include $(BUILD_PREBUILT)