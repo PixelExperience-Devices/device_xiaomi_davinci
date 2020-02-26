c262fa477c3ce431e0d51a52a04261e2ef5e9b16
Alessandro Astone <ales.astone@gmail.com>
2020-02-26 15:09:19 +0100

    davinci: Build and enable fastbootd
    Since OEM fastboot implementation does not allow us to flash
    critical partitions, fastbootd is useful for upgrading firmware.
    
    Change-Id: I0b20600fe7203a7aec19cbea8f6849052585c6ab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,6 +181,7 @@ TARGET_SCREEN_DENSITY := 440
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
