a87dd172d8e796a8a374c23fbfbda6508f2eca0b
TheScarastic <warabhishek@gmail.com>
2020-04-20 02:06:00 +0800

    davinci: parts: Code Cleanup

--- parts/src/org/lineageos/settings/dirac/DiracUtils.java
+++ parts/src/org/lineageos/settings/dirac/DiracUtils.java
@@ -58,7 +58,7 @@ public final class DiracUtils {
 
     protected static void setHifiMode(int paramInt) {
          AudioManager audioManager = mContext.getSystemService(AudioManager.class);
-         audioManager.setParameters("hifi_mode=" + (paramInt == 1 ? true : false));
+         audioManager.setParameters("hifi_mode=" + (paramInt == 1));
          mDiracSound.setHifiMode(paramInt);
     }
 
