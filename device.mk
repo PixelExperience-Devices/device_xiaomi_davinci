0560c2e62fb2d98821271bbadecf66ef5e359751
raysenlau <raysen@live.cn>
2020-05-20 10:02:15 +0800

    davinci: Overlay fstab.qcom

--- device.mk
+++ device.mk
@@ -84,6 +84,9 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc \
     init.power.rc
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/fstab.qcom
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
