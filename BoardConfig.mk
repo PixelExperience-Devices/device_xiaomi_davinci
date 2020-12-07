d622115b1718718a3822248f6a6a46f5578b05f6
Pig <pig.priv@gmail.com>
2020-12-07 10:18:09 +0800

    davinci: Move DPM properties to /system_ext
     * DPM blobs are under /system_ext.
    
    Change-Id: I6bafdc3b310168c6b4d681c9eb94eb4e2e43c5e5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,6 +151,7 @@ TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # QCOM
