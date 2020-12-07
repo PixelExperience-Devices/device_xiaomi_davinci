0dc71446509e1ac792365911e6cec16dcad135c0
Rahul Shahare <rshaha@codeaurora.org>
2020-12-07 21:31:10 +0530

    davinci: Add kpti=off to boot command line
    This will OFF kernel page table isolation.
    
    Notes from Kuba Wojciechowski <nullbytepl@gmail.com> in https://github.com/tadiphone-caf/msm-4.14/commit/86bb20a8325d32ac678573c86874f36ec4575ee2:
    As per qualcomm - "SM8150/SM8250/SM8350/SM7250/SM7150/SM6150 - KPTI Not required".
    It can also help increase performance by a lot in some scenarios.
    
    Change-Id: Idd36c359d4955701e9ee3ee27a58177a8d622c86
    Signed-off-by: Rahul Shahare <rshaha@codeaurora.org>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x880000 loop.max_part=7 cgroup.memory=nokmem,nosocket
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x880000 loop.max_part=7 cgroup.memory=nokmem,nosocket kpti=off
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
