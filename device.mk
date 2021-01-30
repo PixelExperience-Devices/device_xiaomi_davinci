ff05bc31886bfec6a18773ce94b3dcd7d6cec3e3
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-01-30 13:20:51 +0800

    Add atrace-hal build rule
    Change-Id: I28417b074693582b4df14ed8fc5a781909d255f3

--- device.mk
+++ device.mk
@@ -66,6 +66,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# ATRACE_HAL
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
