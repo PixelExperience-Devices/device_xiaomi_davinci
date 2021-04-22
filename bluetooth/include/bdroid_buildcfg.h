f4fde5248e7be3299c1803b3c856cff81be2dfd1
trpsl <trpsl755@gmail.com>
2021-04-23 00:43:00 +0200

    Cleanup buildcfg
    Change-Id: I0e9006cf3db66fe0a22c4f2c9e9a67a5eafbf1d3

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -23,46 +23,6 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#include <stdint.h>
-#include <string.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-int property_get(const char *key, char *value, const char *default_value);
-#ifdef __cplusplus
-}
-#endif
-
-#include "osi/include/osi.h"
-
-typedef struct {
-    const char *product_device;
-    const char *product_model;
-} device_t;
-
-static const device_t devices[] = {
-    {"cepheus", "Xiaomi Mi 9"},
-};
-
-static inline const char *BtmGetDefaultName()
-{
-    char product_device[92];
-    property_get("ro.product.device", product_device, "");
-
-    for (unsigned int i = 0; i < ARRAY_SIZE(devices); i++) {
-        device_t device = devices[i];
-
-        if (strcmp(device.product_device, product_device) == 0) {
-            return device.product_model;
-        }
-    }
-
-    // Fallback to ro.product.model
-    return "";
-}
-
-#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    32
@@ -70,7 +30,6 @@ static inline const char *BtmGetDefaultName()
 #define GATT_MAX_PHY_CHANNEL  10
 // Skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
-// Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec
 #define AVDT_NUM_SEPS 35
 
 #endif
