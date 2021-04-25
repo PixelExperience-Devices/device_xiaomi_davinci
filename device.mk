132247cfd189b7d7afd01f460c339b8f714e5b07
trpsl <trpsl755@gmail.com>
2021-04-25 19:06:54 +0200

    Add thermal config for pixel thermal hal
    https: //android.googlesource.com/device/google/sunfish/+/refs/tags/android-11.0.0_r35/thermal_info_config_sunfish.json
    Change-Id: I6e65abc5453663d203d114af250f0a3f7b7f5b6b

--- device.mk
+++ device.mk
@@ -424,6 +424,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.davinci
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
