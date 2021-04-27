28918ca34930aa5e5b1e1082400adc146b923e7d
trpsl <trpsl755@gmail.com>
2021-04-27 23:17:19 +0200

    Switch to AOSP BT
    This reverts commit bf3c7367587d91ac68d767dc4eb4ce74ef19250e.
    
    Change-Id: Ia6373773b0ca7d630b54ff3162ab6ff6dceac510

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -25,9 +25,8 @@
 
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    32
+#define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-#define GATT_MAX_PHY_CHANNEL  10
-#define AVDT_NUM_SEPS 35
+#define AVDT_NUM_SEPS 12
 
 #endif
