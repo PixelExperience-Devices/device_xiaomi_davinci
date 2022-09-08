119f2838e099fc5e68baf3f9e4680fe99da469a3
Arian <arian.kulmer@web.de>
2022-09-08 15:34:58 +0200

    davinci: Move to legacy-um qcom sepolicy
    Change-Id: I9fa72d7c2313c663490c506780ff4c66ad8037a5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,7 +169,7 @@ TARGET_SCREEN_DENSITY := 440
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
