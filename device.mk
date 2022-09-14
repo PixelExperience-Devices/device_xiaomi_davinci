30ce6a48346c551cb4c97cf48d82ce4cbc6e81a2
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    davinci: Migrate to AIDL ClearKey DRM HAL

--- device.mk
+++ device.mk
@@ -152,7 +152,7 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # Dynamic Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
