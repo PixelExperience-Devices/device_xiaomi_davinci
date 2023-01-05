b4c2a4587adb6e35169ad8d13c666b19ca9aaf4e
althafvly <althafvly@gmail.com>
2023-01-06 00:05:21 +0530

    davinci: init: Set default USB and Bluetooth name
    Change-Id: I65948d08d95b138d9faed262ad3376305b9510a7

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -107,6 +107,8 @@ void vendor_load_properties() {
     set_ro_product_prop("device", device);
     set_ro_product_prop("model", model);
     property_override("ro.build.description", description.c_str());
+    property_override("bluetooth.device.default_name", model.c_str());
+    property_override("vendor.usb.product_string", model.c_str());
     if (mod_device != "") {
         property_override("ro.product.mod_device", mod_device.c_str());
     }
